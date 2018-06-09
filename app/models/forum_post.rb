93bcef91e75b67b70dc7ac3b93864806f03768c7
Linmiao Xu <linmiao.xu@gmail.com>
2018-06-09 12:13:12 +0800

    More forum tweaks

--- app/models/forum_post.rb
+++ app/models/forum_post.rb
@@ -4,8 +4,4 @@ class ForumPost < ApplicationRecord
   validates_presence_of :user_id
   validates_presence_of :title
   validates_presence_of :content
-
-  def user
-    User.find_by(id: user_id)
-  end
 end
