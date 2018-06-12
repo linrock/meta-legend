4403668097c1b3d080363f628ac8dfcd9408380a
Linmiao Xu <linmiao.xu@gmail.com>
2018-06-12 10:10:49 +0800

    Use secure http cookies that expire after 30 days

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: '_metalegend_session'
+Rails.application.config.session_store :cookie_store,
+  key: '_metalegend_session',
+  expire_after: 30.days,
+  httponly: true,
+  secure: true
