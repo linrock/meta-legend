b8dccad7321a6b441e2e1ae8729f12c4491b2d85
Linmiao Xu <linmiao.xu@gmail.com>
2018-06-15 15:51:46 +0800

    Add hearthstone card convenience methods, use cards.collectible.json

--- app/models/hearthstone_card.rb
+++ app/models/hearthstone_card.rb
@@ -1,10 +1,16 @@
-class HearthstoneCard
-  DATA_FILE = "data/cards.json"
+module HearthstoneCard
+  # DATA_FILE = "data/cards.json"
+  DATA_FILE = "data/cards.collectible.json"
 
-  def self.card_map
+  # list of cards
+  def card_data
+    @@card_data ||= open(DATA_FILE, 'r') { |f| JSON.parse(f.read) }
+  end
+
+  # map id -> card info
+  def card_map
     return @@card_map if defined? @@card_map
-    json_data = open(DATA_FILE, 'r').read
-    @@card_map ||= Hash[JSON.parse(json_data).map {|card|
+    @@card_map ||= Hash[card_data.map {|card|
       [card['id'], {
         id: card['id'],
         cost: card['cost'],
@@ -14,18 +20,28 @@ class HearthstoneCard
     }]
   end
 
-  def self.lookup(card_id)
+  def lookup(card_id)
     card_map[card_id]
   end
 
-  def self.card_ids_to_cards(card_ids)
+  def find_by_id(card_id)
+    lookup card_id
+  end
+
+  def find_by_name(card_name)
+    card_data.find {|c| c["name"] == card_name }
+  end
+
+  def card_ids_to_cards(card_ids)
     card_ids.map {|id| lookup(id) }
   end
 
-  def self.card_ids_to_deck_list(card_ids)
+  def card_ids_to_deck_list(card_ids)
     card_ids_to_cards(card_ids)
       .group_by {|card| card[:name] }.values
       .map {|cards| cards[0].merge({ n: cards.length }) }
       .sort_by {|card| [card[:cost], card[:name]] }
   end
+
+  extend self
 end
--- app/models/hearthstone_card_importer.rb
+++ app/models/hearthstone_card_importer.rb
@@ -1,9 +1,9 @@
 require 'open-uri'
 
 class HearthstoneCardImporter
-  API_ENDPOINT = "https://api.hearthstonejson.com/v1/latest/enUS/cards.json"
+  API_ENDPOINT = "https://api.hearthstonejson.com/v1/latest/enUS/cards.collectible.json"
 
-  def import!
+  def self.import!
     json_data = open(API_ENDPOINT, 'r').read
     open(HearthstoneCard::DATA_FILE, 'w') do |f|
       f.write json_data.force_encoding('utf-8')
