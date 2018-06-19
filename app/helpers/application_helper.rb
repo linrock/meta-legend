8e43aea9653169b235123368b6c21034477466ac
Linmiao Xu <linmiao.xu@gmail.com>
2018-06-19 14:24:38 +0800

    Remove unused application helper

--- app/helpers/application_helper.rb
+++
@@ -1,7 +0,0 @@
-module ApplicationHelper
-  def replay_found_at(replay)
-    found_at = time_ago_in_words(replay.created_at)
-    return "minutes ago" if found_at =~ /minute/
-    "#{found_at} ago".gsub(/about\s+/, '')
-  end
-end
