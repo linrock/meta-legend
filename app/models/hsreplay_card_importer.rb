cccfdb4dd1d4df5bb836334a62b6929a599040cf
Linmiao Xu <linmiao.xu@gmail.com>
2018-06-21 20:03:13 +0800

    Clean up hsreplay card importer logic

--- app/models/hsreplay_card_importer.rb
+++ app/models/hsreplay_card_importer.rb
@@ -1,33 +1,44 @@
+require 'open-uri'
+
 class HsreplayCardImporter
 
-  def self.import_all!
+  def self.import_all
     self.hsreplay_card_ids.each do |hsreplay_card_id|
-      self.new(hsreplay_card_id).import_html!
+      self.new(hsreplay_card_id).import_html
     end
   end
 
   # the cards that are used in any given archetype
   def self.hsreplay_card_ids
-    components = Archetype.pluck("
-      data #> '{standard_ccp_signature_core, components}'
-    ")
-    components.flatten.uniq
+    components = Archetype.pluck(
+      Arel.sql("data #> '{standard_ccp_signature_core, components}'")
+    )
+    components.flatten.compact.uniq
   end
 
   def initialize(hsreplay_card_id)
     @hsreplay_card_id = hsreplay_card_id
   end
 
+  def import_html
+    return if File.exists? data_file
+    import_html!
+  end
+
   def import_html!
     begin
       fetched_html = html
-      open("data/hsreplay-cards/#{@hsreplay_card_id}.html", "w") do |f|
+      open(data_file, "w") do |f|
         f.write fetched_html
       end
     rescue => e
       puts "#{e.class.name}: #{e.message}"
       puts "#{e.backtrace.join("\n")}"
     end
+  end
+
+  def data_file
+    "data/hsreplay-cards/#{@hsreplay_card_id}.html"
   end
 
   def html
