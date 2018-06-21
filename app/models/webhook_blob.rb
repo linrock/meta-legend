cf93bbb882ca4543e8e09f4f057bd7b53ee83548
Linmiao Xu <linmiao.xu@gmail.com>
2018-06-21 18:36:19 +0800

    Method to create replay outcomes from webhook blobs

--- app/models/webhook_blob.rb
+++ app/models/webhook_blob.rb
@@ -1,8 +1,44 @@
 class WebhookBlob < ApplicationRecord
-  delegate :friendly_deck_card_ids,
+  delegate :hsreplay_id,
+           :friendly_deck_card_ids,
            :opposing_deck_predicted_card_ids,
+           :winner,
+           :p1_rank, :p1_legend_rank,
+           :p2_rank, :p2_legend_rank,
            :to_replay_data,
            to: :webhook_blob_parser
+
+  def to_replay_outcome_data
+    {
+      id: hsreplay_id,
+      player1_won: winner == 'p1' ? "True" : "False",
+      player2_won: winner == 'p2' ? "True" : "False",
+      player1_rank: p1_rank ? p1_rank : "None",
+      player2_rank: p2_rank ? p1_rank : "None",
+      player1_archetype: p1_archetype_id,
+      player2_archetype: p2_archetype_id,
+      player1_legend_rank: p1_legend_rank ? p1_legend_rank : "None",
+      player2_legend_rank: p2_legend_rank ? p1_legend_rank : "None",
+      source: "webhook #{id}",
+    }
+  end
+
+  def create_replay_outcome!
+    data = to_replay_outcome_data
+    replay_outcome = ReplayOutcome.new({
+      hsreplay_id: data[:id],
+      data: data
+    })
+    replay_outcome.save!
+  end
+
+  def p1_archetype_id
+    Archetype.id_by_archetype_name(friendly_archetype[0][:name])
+  end
+
+  def p2_archetype_id
+    Archetype.id_by_archetype_name(opposing_archetype[0][:name])
+  end
 
   # need to merge this with the data feed somehow
   def replay_data
