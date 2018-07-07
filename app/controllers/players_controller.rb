a66ab1ad045d69ee8cdfccfa74ec4d816b63ff5d
Linmiao Xu <linmiao.xu@gmail.com>
2018-07-07 13:15:22 +0800

    Fix crash due to invalid replays on player pages

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -14,7 +14,7 @@ class PlayersController < ApplicationController
     @title = "#{@player_name} | Players | Meta Legend"
     @meta_desc = "Find recent legend replays where #{@player_name} played a game"
     @replay_data = @hsreplay_ids.map do |hsreplay_id|
-      ReplayDataCache.new.replay_data_hash hsreplay_id
+      ReplayDataCache.new.replay_data_hash(hsreplay_id) rescue nil
     end.compact.sort_by {|r| -r[:found_at].to_i }.to_json
   end
 end
