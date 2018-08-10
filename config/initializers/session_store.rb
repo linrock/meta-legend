eaad79db2a1906f08345f69e0ad39b773b495bcb
Linmiao Xu <linmiao.xu@gmail.com>
2018-08-10 15:18:31 +0700

    Don't require secure cookies in development mode

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,12 @@
-Rails.application.config.session_store :cookie_store,
-  key: '_metalegend_session',
-  expire_after: 30.days,
-  httponly: true,
-  secure: true
+if Rails.env.production?
+  Rails.application.config.session_store :cookie_store,
+    key: '_metalegend_session',
+    expire_after: 30.days,
+    httponly: true,
+    secure: true
+else
+  Rails.application.config.session_store :cookie_store,
+    key: '_metalegend_session',
+    expire_after: 30.days,
+    httponly: true
+end
