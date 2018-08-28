932896a864b298e3c206a80df41e7730d3c3e477
Linmiao Xu <linmiao.xu@gmail.com>
2018-08-28 23:15:17 +0800

    Don't use cached replay data on wild#index page

--- app/controllers/wild_controller.rb
+++ app/controllers/wild_controller.rb
@@ -2,7 +2,6 @@ class WildController < ApplicationController
   PAGE_SIZE = 25
 
   def index
-    @replay_data = JsonResponseCache.new(params).cached_json_response || "{}"
     title_and_description = TitleAndDescription.new(params)
     @title = title_and_description.html_title
     @meta_desc = title_and_description.html_meta_description
