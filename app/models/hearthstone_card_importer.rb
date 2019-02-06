924a7f3dbd640b52032cbd5d615b03805b99b8c4
Linmiao Xu <linmiao.xu@gmail.com>
2019-02-06 23:27:22 +0800

    Add script for importing hearthstone cards data file

--- app/models/hearthstone_card_importer.rb
+++ app/models/hearthstone_card_importer.rb
@@ -4,9 +4,10 @@ class HearthstoneCardImporter
   API_ENDPOINT = "https://api.hearthstonejson.com/v1/latest/enUS/cards.collectible.json"
 
   def self.import!
-    json_data = open(API_ENDPOINT, 'r').read
+    json_data = JSON.parse open(API_ENDPOINT, 'r').read.force_encoding('utf-8')
+    json_data = JSON.pretty_generate(json_data).to_s
     open(HearthstoneCard::DATA_FILE, 'w') do |f|
-      f.write json_data.force_encoding('utf-8')
+      f.write json_data
     end
   end
 end
---
+++ scripts/fetch_cards.rb
@@ -0,0 +1,1 @@
+HearthstoneCardImporter.import!
