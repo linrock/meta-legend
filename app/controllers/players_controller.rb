f7ab24dedf784cefdc4e097c461734e5a0742985
Linmiao Xu <linmiao.xu@gmail.com>
2019-02-10 16:06:59 +0800

    Use solr to search for player games

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,11 +2,8 @@ class PlayersController < ApplicationController
 
   def show
     @player_name = params[:name]
-    @hsreplay_ids = ReplayXmlData
-      .has_player_name(params[:name])
-      .order("played_at DESC")
-      .limit(80)
-      .pluck(:hsreplay_id)
+    # @hsreplay_ids = hsreplay_ids_from_xml_data
+    @hsreplay_ids = hsreplay_ids_from_solr
     if @hsreplay_ids.length == 0
       render status: 404
       return
@@ -16,5 +13,22 @@ class PlayersController < ApplicationController
     @replay_data = @hsreplay_ids.map do |hsreplay_id|
       ReplayDataCache.new.replay_data_hash(hsreplay_id) rescue nil
     end.compact.sort_by {|r| -r[:found_at].to_i }.to_json
+  end
+
+  private
+
+  def hsreplay_ids_from_xml_data
+    ReplayXmlData
+      .has_player_name(params[:name])
+      .order("played_at DESC")
+      .limit(80)
+      .pluck(:hsreplay_id)
+  end
+
+  def hsreplay_ids_from_solr
+    CombinedReplayDataQuery
+      .new({ name: params[:name] })
+      .search_results
+      .results.pluck(:hsreplay_id)
   end
 end
--- app/models/combined_replay_data_query.rb
+++ app/models/combined_replay_data_query.rb
@@ -7,43 +7,65 @@ class CombinedReplayDataQuery
   end
 
   def search_results
-    CombinedReplayData.search do
-      # top 100 legend players
-      top_100 = -> {
-        with(:p1_legend_rank).between(1..100)
-        with(:p2_legend_rank).between(1..100)
-      }
-
-      # top 500 legend players
-      top_500 = -> {
-        with(:pilot_legend_rank).between(1..500)
-        with(:opponent_legend_rank).between(1..500)
-      }
-
-      # top 1000 legend players
-      top_1000 = -> {
-        with(:pilot_legend_rank).between(1..1000)
-        with(:opponent_legend_rank).between(1..1000)
-      }
-
-      # rank 5+ players
-      rank_5 = -> {
-        all do
-          any_of do
-            with(:pilot_legend_rank)
-            with(:pilot_rank).between(1..5)
-          end
-          any_of do
-            with(:opponent_legend_rank)
-            with(:opponent_rank).between(1..5)
-          end
+    query = @query
+    if @query[:battletag]
+      CombinedReplayData.search do
+        any_of do
+          with :p1_battletag, query[:battletag]
+          with :p2_battletag, query[:battletag]
         end
-      }
+        order_by :played_at, :desc
+        paginate page: 1, per_page: 100
+      end
+    elsif @query[:name]
+      CombinedReplayData.search do
+        any_of do
+          with :p1_name, query[:name]
+          with :p2_name, query[:name]
+        end
+        order_by :played_at, :desc
+        paginate page: 1, per_page: 100
+      end
+    else
+      CombinedReplayData.search do
+        # top 100 legend players
+        top_100 = -> {
+          with(:p1_legend_rank).between(1..100)
+          with(:p2_legend_rank).between(1..100)
+        }
+
+        # top 500 legend players
+        top_500 = -> {
+          with(:pilot_legend_rank).between(1..500)
+          with(:opponent_legend_rank).between(1..500)
+        }
+
+        # top 1000 legend players
+        top_1000 = -> {
+          with(:pilot_legend_rank).between(1..1000)
+          with(:opponent_legend_rank).between(1..1000)
+        }
+
+        # rank 5+ players
+        rank_5 = -> {
+          all do
+            any_of do
+              with(:pilot_legend_rank)
+              with(:pilot_rank).between(1..5)
+            end
+            any_of do
+              with(:opponent_legend_rank)
+              with(:opponent_rank).between(1..5)
+            end
+          end
+        }
+      end
     end
   end
 
   private
 
   def sanitize_query(query)
+    query
   end
 end
