81f1be99ff82aa459194abe0145769b15d7cabd7
Linmiao Xu <linmiao.xu@gmail.com>
2019-02-11 13:36:24 +0800

    Get everything working with webpack 4 and babel 7

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,13 @@
 const { environment } = require('@rails/webpacker')
-const typescript =  require('./loaders/typescript')
-const vue =  require('./loaders/vue')
+const { VueLoaderPlugin } = require('vue-loader')
+const vue = require('./loaders/vue')
+const typescript = require('./loaders/typescript')
+const stylus = require('./loaders/stylus')
+const pug = require('./loaders/pug')
 
-environment.loaders.append('vue', vue)
-environment.loaders.append('typescript', typescript)
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', vue)
+environment.loaders.prepend('typescript', typescript)
+environment.loaders.prepend('stylus', stylus)
+environment.loaders.prepend('pug', pug)
 module.exports = environment
