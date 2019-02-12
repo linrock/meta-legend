0cebddd83ecd4808a8583aa0cc37196acad6e14b
Linmiao Xu <linmiao.xu@gmail.com>
2019-02-12 15:16:36 +0800

    Add db columns for pilot pre/post mulligan hands

--- db/migrate/20190208044720_create_combined_replay_data.rb
+++ db/migrate/20190208044720_create_combined_replay_data.rb
@@ -5,23 +5,25 @@ class CreateCombinedReplayData < ActiveRecord::Migration[5.2]
       t.jsonb :metadata, null: false, default: {}
 
       t.string :p1_battletag
-      t.string :p1_class
+      t.string :p1_class, null: false
       t.string :p1_archetype
       t.integer :p1_rank
       t.integer :p1_legend_rank
       t.boolean :p1_is_first
       t.boolean :p1_wins
-      t.jsonb :p1_deck_card_ids
+      t.jsonb :p1_deck_card_ids, null: false, default: []
+      t.jsonb :p1_pre_mulligan_card_ids, null: false, default: []
+      t.jsonb :p1_post_mulligan_card_ids, null: false, default: []
 
       t.string :p2_battletag
-      t.string :p2_class
+      t.string :p2_class, null: false
       t.string :p2_archetype
       t.integer :p2_rank
       t.integer :p2_legend_rank
       t.boolean :p2_is_first
       t.boolean :p2_wins
-      t.jsonb :p2_deck_card_ids
-      t.jsonb :p2_predicted_deck_card_ids
+      t.jsonb :p2_deck_card_ids, null: false, default: []
+      t.jsonb :p2_predicted_deck_card_ids, null: false, default: []
 
       t.string :game_type
       t.integer :ladder_season
