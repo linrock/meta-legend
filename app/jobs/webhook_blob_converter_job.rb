846f52bbbf9785c14736044c4fad91830a63d31a
Linmiao Xu <linmiao.xu@gmail.com>
2019-02-13 09:47:15 +0800

    Fix webhook blob job

--- app/jobs/webhook_blob_converter_job.rb
+++ app/jobs/webhook_blob_converter_job.rb
@@ -6,7 +6,7 @@ class WebhookBlobConverterJob
   sidekiq_options queue: :default, backtrace: true
 
   def perform(webhook_blob_id)
-    blob = WebhookBlob.find_by(webhook_blob_id)
+    blob = WebhookBlob.find_by(id: webhook_blob_id)
     return unless blob.present?
     blob.convert_to_replay_outcome
   end
