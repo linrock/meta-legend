b72c4374ed99327cdd0be5d903f2800f564d1f09
Linmiao Xu <linmiao.xu@gmail.com>
2019-02-14 13:29:05 +0800

    Don't load unused railties

--- config/application.rb
+++ config/application.rb
@@ -3,7 +3,7 @@ require_relative 'boot'
 require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
-require "active_job/railtie"
+# require "active_job/railtie"
 require "active_record/railtie"
 # require "active_storage/engine"
 require "action_controller/railtie"
@@ -11,7 +11,7 @@ require "action_controller/railtie"
 require "action_view/railtie"
 # require "action_cable/engine"
 require "sprockets/railtie"
-require "rails/test_unit/railtie"
+# require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -26,8 +26,6 @@ module MetaLegend
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.active_job.queue_adapter = :sidekiq
-    config.active_job.queue_name_prefix = "metalegend-#{Rails.env}"
 
     config.autoload_paths << Rails.root.join('app/view-models')
 
